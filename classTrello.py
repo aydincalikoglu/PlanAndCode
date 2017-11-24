75b8be88ef142dccbdf466fc951fafd7527fce14
aydincalikoglu <aykocayko@gmail.com>
2017-11-24 10:52:42 +0300

    Exaple Class Type for OOP

---
+++ classTrello.py
@@ -0,0 +1,16 @@
+class trello:
+    def __init__(self,apiKey,TOKEN):
+        self.tokenKey=TOKEN
+        self.apiKey=apiKey
+
+    def createPano(self):
+        print("")
+
+        panoId=""
+        return panoId
+
+    def removePano(self,panoID):
+        print("")
+
+
+
